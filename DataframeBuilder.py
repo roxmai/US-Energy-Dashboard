a832d9c180be5e68b780a49942182a556a23336c
Steven (Han) Au <stevenhcau@gmail.com>
2024-06-07 22:36:19 -0600

    Wrangled df_NGConsumption dataframe in DataframeBuilder.py into format for merging and joining

--- DataframeBuilder.py
+++ DataframeBuilder.py
@@ -1,8 +1,20 @@
 import pandas as pd
 import numpy as np
+import datetime as date
 
-dfElectrictyConsumption = pd.read_csv('Retail_sales_of_electricity.csv', header = 4, sep=',')
-dfNaturalGasConsumption = pd.read_excel('NG_CONS_SUM_DCU_NUS_M.xlsx', sheet_name=1, header=2)
+
+df_EConsumption = pd.read_csv('Retail_sales_of_electricity.csv', header = 4, sep=',')
+df_NGConsumption = pd.read_excel('NG_CONS_SUM_A_EPG0_VGT_MMCF_M.xls', sheet_name=1, header=2)
 dfHDD = pd.read_csv('MER_T01_10.csv', sep = ',')
 dfCDD = pd.read_csv('MER_T01_11.csv', sep = ',')
-print(dfCDD)
+
+# Modify header row in NG Consumption dataset
+
+df_NGConsumption.columns = df_NGConsumption.columns.map(lambda x: str.replace(x, ' (Including Vehicle Fuel) (MMcf)', ''))
+df_NGConsumption.columns = df_NGConsumption.columns.map(lambda x: str.replace(x, 'Natural Gas Delivered to Consumers in ', ''))
+df_NGConsumption = df_NGConsumption.melt(id_vars = ['Date'], var_name='State', value_name='Natural Gas Consumption (MMcf)')
+
+# print(df_NGConsumption.head(15))
+print(df_NGConsumption.dtypes)
+
+
