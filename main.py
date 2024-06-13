75d0b34acb32360184bb3ad2dd8bcef5009a7c8f
rmai <rmai.aickom@gmail.com>
2024-06-12 23:29:59 -0600

    add comments for script planning

--- main.py
+++ main.py
@@ -3,9 +3,19 @@ from analysis import analyze_data
 import numpy as np
 import pandas as pd
 
-def main():
+# def main():
     # build the dataframe using the provided function
-    df = build_data_frame()
+df = build_data_frame()
 
-if __name__ == '__main__':
-    main()
+# Ask user for region input
+region_input = input("Enter region that you want analysis? (e.g. East North Central):").strip().lower()
+
+# Ask user for date input
+year_input = input("Do you want to analyze a specific year or a range")
+
+# Perform analysis based on user inputs
+
+# Plot energy comsumption data
+
+# if __name__ == '__main__':
+#     main()
