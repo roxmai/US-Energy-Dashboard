2d722674bb4b887f0d8392c0b24bab507f2998bf
rmai <rmai.aickom@gmail.com>
2024-06-12 23:08:51 -0600

    inital update

---
+++ main.py
@@ -0,0 +1,11 @@
+from DataframeBuilder import build_data_frame
+from analysis import analyze_data
+import numpy as np
+import pandas as pd
+
+def main():
+    # build the dataframe using the provided function
+    df = build_data_frame()
+
+if __name__ == '__main__':
+    main()
