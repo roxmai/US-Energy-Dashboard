f59bf78cb8d53be077459b4a3dfb69f2b3e119e9
rmai <rmai.aickom@gmail.com>
2024-06-13 14:10:35 -0600

    initial update

---
+++ src/Plot.py
@@ -0,0 +1,11 @@
+def plot_energy_consumption(df):
+    
+    df = df.reset_index()
+
+    # Plot Natural Gas Vonsumption
+
+    # Plot Rlectrical Consumption
+
+    # Plot Total Energy Consumption
+
+    # Plot Hheating Degree Days (HDD)
