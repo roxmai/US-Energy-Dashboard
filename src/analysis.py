800b76beccf7f3acd48730a0b3f6ad9eab73fbb2
rmai <rmai.aickom@gmail.com>
2024-06-16 17:08:27 -0600

    merged with main branch and add plots to src Plot.py

--- src/analysis.py
+++ src/analysis.py
@@ -1,15 +1,28 @@
 import numpy as np
 import pandas as pd
+# New function adding features
 
 def analyze_data(df):
+    print(df.describe())
+
     # Adding Total Energy Consumption, Natural Gas Energy Consumption, and Electrical/Natural Gas Ratio column
-    print('\n'*2)
-    print('******** Natural Gas, Electricity, Total Energy Consumption, Degree Days By State By Month,Year ********')
     natural_gas_energy_density = 0.303634232 # million kWh per 1 MMcf, from EIA conversion calculator. assuming natural gas is in standard volume
     df['Natural Gas Energy Consumption (million kWh)'] = df['Natural Gas Consumption (MMcf)']*natural_gas_energy_density 
     df['Total Energy Consumption (million kWh)'] = df['Natural Gas Energy Consumption (million kWh)']+df['Electrical Consumption (million kWh)']
-    df['Electrical/Natural Gas Consumption Ratio'] = df['Electrical Consumption (million kWh)']/df['Natural Gas Energy Consumption (million kWh)']
-    print(df.head(5))
+    df['Electrical/Natural Gas Consumption Ratio'] = df['Electrical Consumption (million kWh)'] / df['Natural Gas Energy Consumption (million kWh)']
+    print(df)
+
+    # ******
+    new_df = df.pivot_table("Natural Gas Consumption (MMcf)", index=["Month"], columns=["Year"])
+
+    df["DateTime"] = df.index.get_level_values('Year').astype(str) + '-' + df.index.get_level_values('Month').astype(str)
+    df["DateTime"] = pd.to_datetime(df["DateTime"], format="%Y-%m")
+
+    total_energy_df = df.pivot_table("Total Energy Consumption (million kWh)", index=["DateTime"])
+    elec_consumption_df = df.pivot_table('Electrical Consumption (million kWh)', index = ['DateTime'])
+    natural_gas_energy_df = df.pivot_table('Natural Gas Energy Consumption (million kWh)', index = ['DateTime'])
+    dd_df = df.pivot_table('DD', index = ['DateTime'])
+    df_year = df.pivot_table("Total Energy Consumption (million kWh)", index = 'Month', columns = 'Year')
 
     # Total Consumption Over all years
     print('\n'*2)
@@ -32,13 +45,12 @@ def analyze_data(df):
     print(grouped_months_df)
 
     # Average Monthly Consumption by Season
-    print('\n'*2)
+    print('\n' * 2)
     print('******** Average Monthly Consumption and Degree Days by Season ********')
-    winter_averages = grouped_months_df.loc[[12,1,2]].mean()
-    spring_averages = grouped_months_df.loc[[3,4,5]].mean()
-    summer_averages = grouped_months_df.loc[[6,7,8]].mean()
-    fall_averages = grouped_months_df.loc[[9,10,11]].mean()
-    seasonal_averages = pd.DataFrame({'Winter': winter_averages, 'Spring':spring_averages, 'Summer': summer_averages, 'Fall': fall_averages}).transpose()
+    winter_averages = grouped_months_df.loc[[12, 1, 2]].mean()
+    spring_averages = grouped_months_df.loc[[3, 4, 5]].mean()
+    summer_averages = grouped_months_df.loc[[6, 7, 8]].mean()
+    fall_averages = grouped_months_df.loc[[9, 10, 11]].mean()
+    seasonal_averages = pd.DataFrame({'Winter': winter_averages, 'Spring': spring_averages, 'Summer': summer_averages, 'Fall': fall_averages}).transpose()
     print(seasonal_averages)
-    
     return df
